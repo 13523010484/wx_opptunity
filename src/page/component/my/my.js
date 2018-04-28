a448a6f968fb5f9ff30088b8f05e7c73a9143773
jiaqiuhua <flower346@qq.com>
2018-04-28 18:17:55 +0800

    完善登录逻辑

--- src/page/component/my/my.js
+++ src/page/component/my/my.js
@@ -18,5 +18,38 @@ Page({
                 })
             }
         })
+    },
+    getNewVersion: function () {
+        const updateManager = wx.getUpdateManager()
+
+        updateManager.onCheckForUpdate(function (res) {
+            // 请求完新版本信息的回调
+            console.log(res.hasUpdate)
+            if (res.hasUpdate) {
+                updateManager.onUpdateReady(function () {
+                    wx.showModal({
+                        title: '更新提示',
+                        content: '新版本已经准备好，是否重启应用？',
+                        success: function (res) {
+                            if (res.confirm) {
+                                // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                                updateManager.applyUpdate()
+                            }
+                        }
+                    })
+
+                })
+            } else {
+                wx.showModal({
+                    title: '提示',
+                    content: '当前已经是最新版本了哦~',
+                    showCancel: false
+                })
+            }
+        })
+
+        updateManager.onUpdateFailed(function () {
+            // 新的版本下载失败
+        })
     }
 })
