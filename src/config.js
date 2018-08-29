c53c3cfdca404fe89864cd55ae8c47b31680f825
JF <flower346@qq.com>
2018-08-29 08:56:57 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,6 @@
 /**
  * 小程序配置文件
  */
-var host = "https://cms.orenda.com.cn"
-// var host = "http://192.16.8.40:8081"// 海斌
-// var host = "http://192.16.8.34:8082"// 克进
-// var host = "http://192.16.8.37:8082"// 宁宁
 
 
 var config = {
